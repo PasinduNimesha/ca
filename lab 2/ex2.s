d2e5cf92624e04e6eae7ecf9bb910075ce31acba
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2023-08-18 18:45:48 +0530

    changed ex2, added commets

--- lab 2/ex2.s
+++ lab 2/ex2.s
@@ -1,45 +1,47 @@
 .data
-N:        .word        12
-A:        .word        2, 3, 5, -7, 8, -4, 6, 13, -11, 2, 5, 1
-B:        .word        0, 1, 2, 7, -8, 4, 5, 12, 11, -2, 6, 3
-C:        .word        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-newline:  .string      "\n"
+N:        .word        12           # Number of elements in the arrays
+A:        .word        2, 3, 5, -7, 8, -4, 6, 13, -11, 2, 5, 1   # Array A
+B:        .word        0, 1, 2, 7, -8, 4, 5, 12, 11, -2, 6, 3    # Array B
+C:        .word        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0        # Array C to store results
+newline:  .string      "\n"          # Newline string for printing
 
 .text
 .globl main
 
 main:
-    li a2, 2
-    la t0, A    # Load address of vector A into t0
-    la t1, B    # Load address of vector B into t1
-    la t2, C    # Load address of vector B into t2
-    lw t3, N    # Load N into t3
+    li a2, 2            # Load the divisor (2) for checking even numbers
+    la t0, A            # Load address of array A into t0
+    la t1, B            # Load address of array B into t1
+    la t2, C            # Load address of array C into t2
+    lw t3, N            # Load the number of elements into t3
 
 loop:
-    lw t4, 0(t0)        # Load the current element of A into t4
-    lw t5, 44(t1)
-    
-    add t6, t4, t5
-    sw t3, 0(t6)
-    j next_iteration    # Branch to check_positive if t3 > 0
+    lw t4, 0(t0)        # Load the current element of array A into t4
+    lw t5, 44(t1)       # Load the corresponding element of array B into t5 (offset 44 because each element is 4 bytes)
+
+    add t6, t4, t5      # Calculate the sum of A[i] + B[i]
+    sw t6, 0(t2)        # Store the result in array C
+
+    j next_iteration     # Jump to next_iteration
 
 next_iteration:
-    addi t3, t3, -1    # Decrement the loop counter
-    addi t0, t0, 4     # Move to the next element in A
-    addi t1, t1, -4    # Move to the next element in B
-    
-    add a0, a0, t6
-    li a7, 1
-    ecall
-    
-    la a0, newline
-    li a7, 4
-    ecall
-    li a0, 0
-    
-    bnez t3, loop    # Branch to loop if t3 != 0
-    j end
+    addi t3, t3, -1     # Decrement the loop counter
+    addi t0, t0, 4      # Move to the next element in array A
+    addi t1, t1, -4     # Move to the previous element in array B
+    addi t2, t2, 4      # Move to the next position in array C
 
+    add a0, a0, t6      # Add the current result to a0 (for printing)
+    li a7, 1            # System call number for printing an integer
+    ecall               # Make the system call to print
+
+    la a0, newline      # Load the newline string for printing
+    li a7, 4            # System call number for printing a string
+    ecall               # Make the system call to print
+
+    li a0, 0            # Reset a0 for further iterations
+
+    bnez t3, loop       # Branch to loop if t3 != 0
+    j end               # Jump to the end
 
 end:
-    j end
+    ret
--- lab 2/ex3.s
+++ lab 2/ex3.s
@@ -1,37 +1,47 @@
 .data
-n:             .word        12
-f0:            .word         0
-f1:            .word         1
-array:         .word         0
-
+N:        .word        12           # Number of elements in array A
+A:        .word        0, 1, 2, 7, -8, 4, 5, 12, 11, -2, 6, 3   # Array A
+B:        .word        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0       # Array B to store even positive numbers
+newline:  .string      "\n"          # Newline string for printing
 
 .text
 .globl main
 
-main:  
-    la t0, array 
-    lw t4, n
-    lw t1, f0 
-    lw t2, f1
-    
-    sw t1, 0(t0)
-    add t3, t1, x0
-    addi t4, t4, -1
-    sw t2, 0(t0)
-    add t3, t2, x0
-    addi t4, t4, -1
-    
+main:
+    li a2, 2            # Load the divisor (2) for checking even numbers
+    la t0, A            # Load address of array A into t0
+    la t1, B            # Load address of array B into t1
+    lw t2, N            # Load the number of elements into t2
 
 loop:
-    add t3, t2, t1
-    add t1, t2, x0
-    add t2, t3, x0
-    sw t3, 0(t0)
-    addi t0, t0, 4
-    addi t4, t4, -1
+    lw t3, 0(t0)        # Load the current element of A into t3
+    bgtz t3, check_positive  # Branch to check_positive if t3 > 0
 
-    bnez t4, loop
-    j end
-   
+next_iteration:
+    addi t2, t2, -1     # Decrement the loop counter
+    addi t0, t0, 4      # Move to the next element in A
+    bnez t2, loop       # Branch to loop if t2 != 0
+    j end               # Jump to the end
+
+check_positive:
+    rem t4, t3, a2      # Calculate the remainder of t3 divided by 2
+    bnez t4, next_iteration  # If remainder is not zero, skip to next iteration
+    
+    add a0, a0, t3      # Add the even positive number to a0 (for printing)
+    li a7, 1            # System call number for printing an integer
+    ecall               # Make the system call to print
+    
+    la a0, newline      # Load the newline string for printing
+    li a7, 4            # System call number for printing a string
+    ecall               # Make the system call to print
+    
+    li a0, 0            # Reset a0 for further iterations
+    
+    sw t3, 0(t1)         # Store the even positive number into array B
+    addi t1, t1, 4      # Move to the next position in B
+    j next_iteration     # Continue with the next iteration
+
 end:
-    ret
+    j end               # Jump to the end
+
+
